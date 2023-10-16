15be8e4a8c3a11a824faebb7df57bcb0fae0d872
HelloWorld7894 <danpojhan@protonmail.com>
2023-10-16 12:23:21 +0200

    added simple project structure

---
+++ src/gen_map/train/model.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+###
+###VERSION 1
+###
+
+
+class GridGeneration(): #generate grid
+    def __init__(self):
+        super(GridGeneration, self).__init__()
+
+###GRADIENT GENERATION
+class BilinearGGA: #gradient generation algorithm
+    def __init__(self):
+        pass
+
+class BicubicGGA:
+    def __init__(self):
+        pass
+
+class cubicSplineGGA:
+    def __init__(self):
+        pass
+
+class GGN(nn.Module): #gradient generation network
+    def __init__(self):
+        super(GGN, self).__init__()
+    
+    def forward(self):
+        pass
+
+
+###
+###MAIN CODE
+###
+
+def train():
+    pass
+
+def test():
+    pass
+
+def valid():
+    pass
