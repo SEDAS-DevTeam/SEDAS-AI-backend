7916297785585416e5d216dd11936ec0acc55f3d
HelloWorld7894 <danpojhan@protonmail.com>
2023-11-09 19:04:04 +0100

    speech model finally works

--- src/SpeechSynthesis/speech_models.py
+++ src/SpeechSynthesis/speech_models.py
@@ -14,9 +14,11 @@ class Google:
         self.gtts.write_to_fp(self.bytes_obj)
 
     def play_audio(self):
+        self.bytes_obj.seek(0)
         song = AudioSegment.from_file(self.bytes_obj, sample_width=2, frame_rate=44100, channels=1)
         play(song)
 
 if __name__ == "__main__":
-    speech_synth = Google(text="Fly heading 090")
+    speech_synth = Google(text="Fly heading zero niner zero")
     speech_synth.process()
+    speech_synth.play_audio()
