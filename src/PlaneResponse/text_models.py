99cd211d4896635301a436e90864ccd5eeb7256e
HelloWorld7894 <danpojhan@protonmail.com>
2023-12-25 01:18:40 +0100

    rewrote text model for database

--- src/PlaneResponse/text_models.py
+++ src/PlaneResponse/text_models.py
@@ -4,7 +4,8 @@ class simplePOS:
     #really simple POS tagging algorithm that works only for: "fly heading" commands, USE ONLY FOR DEVELOPEMENT
     #requires installing spacy pretrained english model using this command: python3 -m spacy download en_core_web_sm
 
-    def __init__(self):
+    def __init__(self, db_instance):
+        self.db_instance = db_instance
         self.nlp = spacy.load("en_core_web_sm")
 
     def heading_translation(self, head_text):
@@ -13,30 +14,38 @@ class simplePOS:
         if head[0] == "0": return int(head[1:])
         else: return int(head)
 
-    def process(self, text):
-        doc = self.nlp(text)
+    def process(self):
+        last_value = ""
+        while True:
+            text = self.db_instance.get("proc-voice")
+            if text != last_value:
+                #onchange
 
-        value = 0
-        name = ""
-        command = ""
+                doc = self.nlp(text)
 
-        for i, token in enumerate(doc):
-            #searching for fly-heading command
-            if token.text == "fly" and doc[i + 1].text == "heading":
-                command = "change-heading"
+                value = 0
+                name = ""
+                command = ""
 
-        for i, token in enumerate(doc):
-            if command == "change-heading":
-                #searching for heading
-                if token.pos_ == "NUM" and doc[i - 1].text == "heading":
-                    value = self.heading_translation(token.text)
-                    continue
-                
-                #searching for plane id
-                if any(char.isdigit() for char in token.text):
-                    name = token.text
-        
-        return name, command, value
+                for i, token in enumerate(doc):
+                    #searching for fly-heading command
+                    if token.text == "fly" and doc[i + 1].text == "heading":
+                        command = "change-heading"
+
+                for i, token in enumerate(doc):
+                    if command == "change-heading":
+                        #searching for heading
+                        if token.pos_ == "NUM" and doc[i - 1].text == "heading":
+                            value = self.heading_translation(token.text)
+                            continue
+                        
+                        #searching for plane id
+                        if any(char.isdigit() for char in token.text):
+                            name = token.text
+
+                if len(name) != 0 and len(command) != 0:
+                    self.db_instance.set("proc-voice-out", f"{name} {command} {value}")
+
 
 if __name__ == "__main__":
     pos = simplePOS()
