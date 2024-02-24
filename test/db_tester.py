d266bb97a7db2f22113a8361c67a092ebd52cce2
HelloWorld7894 <danpojhan@protonmail.com>
2024-02-24 22:57:37 +0100

    db tester redesign

--- test/db_tester.py
+++ test/db_tester.py
@@ -1,14 +1,13 @@
+# Import Module 
 import tkinter as tk
+import time 
 import threading
-import time
 import redis
-import atexit
-
+  
 main = tk.Tk()
 main.title("SEDAS DB-tester")
 main.geometry("500x200")
 
-#variables
 out_text = tk.StringVar()
 out_voice = tk.StringVar()
 out_speech = tk.StringVar()
@@ -17,83 +16,60 @@ error_message_text = tk.StringVar()
 running = False
 terminate = False
 CATCH_TIMEOUT = 1 #s
+thread = None
 
 out_text.set("Not running")
 out_voice.set("Not running")
 out_speech.set("Not running")
 start_stop.set("START")
 
-#other declarations
-class DB_catcher(threading.Thread):
-    def __init__(self, database):
-        threading.Thread.__init__(self)
-        self.database = database
-
-    def run(self):
-        global running, terminate
-        while True:
-            if terminate:
-                self.database.close()
-                print("not terminated")
-                break
-
-            if running:
-                time.sleep(CATCH_TIMEOUT)
-
-                try:
-                    voice = self.database.get("out-voice")
-                    speech = self.database.get("out-speech")
-                    text = self.database.get("out-text")
-
-                    out_voice.set(voice)
-                    out_speech.set(speech)
-                    out_text.set(text)
-
-                    #everything is fine
-                    if error_message_text.get() == "Error connecting to database!":
-                        error_message_text.set("")
-                except redis.ConnectionError:
-                    print("connection error!")
-                    error_message_text.set("Error connecting to database!")
-
-                    running = False
-                    start_stop.set("START")
-
-def start_stop_DB_catch():
-    global running
+#connect to database
+database = redis.Redis(host='localhost', decode_responses=True)
+  
+def thread_run(): 
+    global running, thread, terminate
     if running:
         running = False
         start_stop.set("START")
         out_text.set("Not running")
         out_voice.set("Not running")
         out_speech.set("Not running")
+
+        terminate = True
     else:
         running = True
+        terminate = False
         start_stop.set("STOP")
 
-    print(running)
-
+            # Call work function 
+        thread=threading.Thread(target=db_catcher) 
+        thread.start() 
+  
 def reset_values():
     database.set("out-voice", "None")
     database.set("out-speech", "None")
     database.set("out-text", "None")
 
+# work function 
+def db_catcher(): 
+    global running
+  
+    while True:
+        time.sleep(CATCH_TIMEOUT)
+        
+        if terminate:
+            break
 
+        voice = database.get("out-voice")
+        speech = database.get("out-speech")
+        text = database.get("out-text")
 
-def get_values():
-    pass
-
-def proper_exit():
-    global terminate
-    terminate = True
-    time.sleep(1)
-    catcher_thread.join()
-
+        out_voice.set(voice)
+        out_speech.set(speech)
+        out_text.set(text)
+  
 #connect to database
 database = redis.Redis(host='localhost', decode_responses=True)
-
-catcher_thread = DB_catcher(database)
-catcher_thread.start()
 
 label_text = tk.Label(main, text="TEXT MODEL OUTPUT", borderwidth=1, bg="red")
 label_voice = tk.Label(main, text="VOICE MODEL OUTPUT", borderwidth=1, bg="red")
@@ -119,16 +95,13 @@ frame_text.grid(row=1, column=0, sticky="nsew")
 frame_voice.grid(row=1, column=1, sticky="nsew")
 frame_speech.grid(row=1, column=2, sticky="nsew")
 
-start_stop_button = tk.Button(main, textvariable=start_stop, width=15, command=start_stop_DB_catch)
+start_stop_button = tk.Button(main, textvariable=start_stop, width=15, command=thread_run)
 start_stop_button.grid(row=2, column=0, pady=(10))
 reset_button = tk.Button(main, text="RESET", width=15, command=reset_values)
 reset_button.grid(row=2, column=1, pady=(10))
 
 error_message = tk.Label(main, textvariable=error_message_text, fg="red")
 error_message.grid(row=3, column=0, columnspan=2)
-
-#main code
-main.protocol("WM_DELETE_WINDOW", proper_exit)
 
 main.grid_columnconfigure(0, weight=1)
 main.grid_columnconfigure(1, weight=1)
