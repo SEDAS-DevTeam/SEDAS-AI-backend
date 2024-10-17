bf404e84214b050bffe96c751c0cf008ae368034
HelloWorld7894 <danpojhan@protonmail.com>
2024-10-17 08:53:55 +0200

    working with thred

--- src/test.cpp
+++ src/test.cpp
@@ -12,18 +12,39 @@
 
 class Base {
     public: 
+        bool running = true;
+        bool thread_stop = true;
+
+        void start(){
+            thread_stop = false;
+        }
+
+        void end(){
+            running = false;
+        }
+
+        void stop(){
+            thread_stop = true;
+        }
+
         void run(){
-            sleep(2);
-            std::cout << "I am running!" << std::endl;
+            while(running){
+                if (!thread_stop){
+                    sleep(1);
+                    std::cout << "I am running!" << std::endl;
+                }
+            }
         }
 };
 
 int main(){
     Base base;
     std::thread thread_main(&Base::run, &base);
+    base.start();
 
     sleep(10); //Wait for 10 seconds
 
+    base.end();
     thread_main.join();
     return 0;
 }
